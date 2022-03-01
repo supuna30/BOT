10c9e48b09c62622fc1e89a0a6e4b6dc90487cca
supuna <92195424+supuna28@users.noreply.github.com>
2022-03-01 21:32:18 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -427,7 +427,7 @@ module.exports = {
                                 console.error(e)
                             }
                         }
-                        if (m.limit) m.reply(+ m.limit + ' Limit terpakai')
+                        if (m.limit) m.reply(+ m.limit + ' please wait')
                     }
                     break
                 }
