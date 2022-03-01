33008e72daccde92efefa21d02dde265736e61bd
supuna <92195424+supuna28@users.noreply.github.com>
2022-03-01 20:49:32 +0530

    Update database.js

--- plugins/database.js
+++ plugins/database.js
@@ -1,7 +1,7 @@
 let handler = async (m) => {
     let totalreg = Object.keys(global.db.data.users).length
     let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    m.reply(`*Jumlah database saat ini ${totalreg} user*`)
+    m.reply(`බොට් බාවිතා කරන ගනන ${totalreg} කි*`)
 }
 handler.help = ['database', 'user']
 handler.tags = ['info']
