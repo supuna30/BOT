400e793b315d25296275aad86720dbc0a280991c
supuna <92195424+supuna28@users.noreply.github.com>
2022-03-02 12:50:46 +0530

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -1,6 +1,6 @@
 const {sticker5, sticker} = require('../lib/sticker')
 let handler = async (m, {conn, text}) => {
-  if (!text) throw ('mana teksnya')
+  if (!text) throw ('ටෙස්ක්ස්ට් එකක් ඇතුලත් කරන්න')
   api = await conn.getBuffer(`https://xteam.xyz/attp?file&text=${text}`)
   conn.sendMessage(m.chat, {sticker:api},{quoted:m}) 
 }
