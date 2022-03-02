abce63f2fa2c9ea083946682e56429ec42c177ea
supuna <92195424+supuna28@users.noreply.github.com>
2022-03-02 13:15:13 +0530

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -8,7 +8,7 @@ handler.before = async function (m, { isAdmin, isBotAdmin }) {
   if (chat.antiLink && ValidLink && !isAdmin && !m.isBaileys && m.isGroup) {
     let thisGroup = isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}` : 0
     if (m.text.includes(thisGroup) && thisGroup != 0) throw false // jika link grup itu sendiri gak dikick
-    await conn.reply(m.chat, `*Link Terdeteksi!*${isBotAdmin ? '' : '\n\nbukan admin jadi gabisa kick t_t'}\n\nKetik *.off antilink* untuk mematikan fitur ini${opts['restrict'] ? '' : '\nketik *.on restrict* supaya bisa kick'}`, '', '', '', m)
+    await conn.reply(m.chat, `*Link හමුවිය 😒!*${isBotAdmin ? '' : 'ඉවත් කරන්න 🤤'}\n\nයොදන්න*.off antilink* මෙම විශේෂාංගය අක්‍රිය කිරීමට${opts['restrict'] ? '' : '\nයොදන්න.on restrict* ඒ නිසා ඉවත් කරන්න'}`, '', '', '', m)
     if (global.opts['restrict']) {
       if (isBotAdmin) this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
     }
