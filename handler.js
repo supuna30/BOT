623f723d2a784aa9a9617a320958344461ec604d
supuna <92195424+supuna28@users.noreply.github.com>
2022-03-02 20:29:43 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -378,7 +378,7 @@ module.exports = {
                     if (xp > 200) m.reply('Ngecit -_-') // Hehehe
                     else m.exp += xp
                     if (!isPrems && plugin.limit && global.db.data.users[m.sender].limit < plugin.limit * 1) {
-                        this.reply(m.chat, `Limit anda habis, silahkan beli melalui *${usedPrefix}buy*`, m)
+                        this.reply(m.chat, `ඔබට අද දිනට බොට් භාවිතා කල හැකි ලිමිට් ප්‍රමානය ඉක්මවා ගොස් ඇත *${usedPrefix}buy*`, m)
                         continue // Limit habis
                     }
                     if (plugin.level > _user.level) {
