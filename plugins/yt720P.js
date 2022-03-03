db13a37040536380d1c1c21b6b6f8cfc569d0395
supuna <92195424+supuna28@users.noreply.github.com>
2022-03-03 19:27:07 +0530

    Create yt720P.js

---
+++ plugins/yt720P.js
@@ -0,0 +1,25 @@
+let xfar = require('xfarr-api')
+
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, command, text }) => {
+
+    if (!text) throw 'LINK'
+
+  let res = await xfar.Youtube(text)
+
+m.reply('*WAIT 😎...*')
+
+conn.sendFile(m.chat,res.medias[2].url, '', `Youtube Downloader
+
+720p`, m)
+
+}
+
+handler.help = ['ytv720 <url>']
+
+handler.tags = ['internet']
+
+handler.command = /^ytv720p$/i
+
+module.exports = handler
