f83dd06063dc285acb59ff3ccf2a6f4eb2f923e9
supuna <92195424+supuna28@users.noreply.github.com>
2022-03-07 18:11:57 +0530

    Create song.js

---
+++ plugins/song.js
@@ -0,0 +1,51 @@
+import { youtubeSearch } from '@bochilteam/scraper'
+
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+
+  if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
+
+  let vid = (await youtubeSearch(text)).video[0]
+
+  if (!vid) throw 'Video/Audio සොයා ගත නොහැක'
+
+  let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
+
+  const url = 'https://www.youtube.com/watch?v=' + videoId
+
+  await conn.sendHydrated(m.chat, `
+
+📌 *Title:* ${title}
+
+🔗 *Url:* ${url}
+
+🖹 *Description:* ${description}
+
+⏲️ *Published:* ${publishedTime}
+
+⌚ *Duration:* ${durationH}
+
+👁️ *Views:* ${viewH}
+
+  `.trim(), author, thumbnail, url, '📺 Youtube!', null, null, [
+
+    ['Audio 🎧', `${usedPrefix}yta ${url} yes`], 
+
+    ['Video 🎥', `${usedPrefix}ytv ${url} yes`],
+
+    ['Youtube Search🔎', `${usedPrefix}yts ${url}`]
+
+  ], m)
+
+}
+
+handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
+
+handler.tags = ['downloader']
+
+handler.command = /^play2?$/i
+
+handler.exp = 0
+
+handler.limit = false
+
+export default handler
